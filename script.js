a88aac4bce42e4787827a8b82d4e5a4951c23bfe
Arazo_Leo <151800577+Arazoleo@users.noreply.github.com>
2024-09-29 22:24:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 document.getElementById("but").addEventListener("click", function() {
-    // Adiciona a classe de animação de saída
+   
     document.getElementById("content").classList.add("exiting");
     document.getElementById("but").classList.add("exiting");
     document.body.classList.add("exiting");
 
-    // Após 1s, ativa o novo fundo amarelo e esconde os elementos antigos
+   
     setTimeout(() => {
         document.getElementById("newBackground").classList.add("active");
-    }, 1000); // Correspondente ao tempo de animação
+    }, 1000); 
 });
 
 function AtivaImagem(id){
